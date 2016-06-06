75afb27aed678dfd03690c2baa3b8dcc12ea47fb
Simon Templer <simon@wetransform.to>
2016-06-06 16:56:55 +0200

    backup log in stdout

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@ if [[ "$RESTORE" == "true" ]]; then
   ./restore.sh
 else
   if [ -n "$CRON_TIME" ]; then
+    LOGFIFO='/var/log/cron.fifo'
+    if [[ ! -e "$LOGFIFO" ]]; then
+        mkfifo "$LOGFIFO"
+    fi
     env | grep -v 'affinity:container' | sed -e 's/^\([^=]*\)=\(.*\)/export \1="\2"/' > /env.conf # Save current environment
-    echo "${CRON_TIME} . /env.conf && /backup.sh >> /dockup.log 2>&1" > /crontab.conf
+    echo "${CRON_TIME} . /env.conf && /backup.sh >> $LOGFIFO 2>&1" > /crontab.conf
     crontab  /crontab.conf
     echo "=> Running dockup backups as a cronjob for ${CRON_TIME}"
-    exec cron -f
+    cron
+    tail -f "$LOGFIFO"
   else
     ./backup.sh
   fi
