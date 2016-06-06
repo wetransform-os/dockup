e19437da9b1c944873241b0e9189c0f01dfb7ec6
Simon Templer <simon@wetransform.to>
2016-06-06 18:20:30 +0200

    more verbose backup script, non-zero exit code on failure

--- backup.sh
+++ backup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 
+function cleanup {
+  # If a post-backup command is defined (eg: for cleanup)
+  if [ -n "$AFTER_BACKUP_CMD" ]; then
+    eval "$AFTER_BACKUP_CMD"
+  fi
+}
+
+start_time=`date +%Y-%m-%d\\ %H:%M:%S\\ %Z`
+echo "[$start_time] Initiating backup $BACKUP_NAME..."
+
 # Get timestamp
 : ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
 readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
@@ -11,22 +21,43 @@ if [ -n "$BEFORE_BACKUP_CMD" ]; then
 fi
 
 # Create a gzip compressed tarball with the volume(s)
-tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
+time tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
+rc=$?
+if [ $rc -ne 0 ]; then
+  echo "ERROR: Error creating backup archive"
+  # early exit
+  cleanup
+  end_time=`date +%Y-%m-%d\\ %H:%M:%S\\ %Z`
+  echo -e "[$end_time] Backup failed\n\n"
+  exit $rc
+else
+  echo "Created archive $tarball"
+fi
 
-# Create bucket, if it doesn't already exist
-BUCKET_EXIST=$(aws s3 --region $AWS_DEFAULT_REGION ls | grep $S3_BUCKET_NAME | wc -l)
-if [ $BUCKET_EXIST -eq 0 ];
-then
-  aws s3 --region $AWS_DEFAULT_REGION mb s3://$S3_BUCKET_NAME
+# Create bucket, if it doesn't already exist (only try if listing is successful - access may be denied)
+BUCKET_LS=$(aws s3 --region $AWS_DEFAULT_REGION ls)
+if [ $? -eq 0 ]; then
+  BUCKET_EXIST=$(echo $BUCKET_LS | grep $S3_BUCKET_NAME | wc -l)
+  if [ $BUCKET_EXIST -eq 0 ];
+  then
+    aws s3 --region $AWS_DEFAULT_REGION mb s3://$S3_BUCKET_NAME
+  fi
 fi
 
 # Upload the backup to S3 with timestamp
-aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$tarball
+echo "Uploading the archive to S3..."
+time aws s3 --region $AWS_DEFAULT_REGION cp $tarball s3://$S3_BUCKET_NAME/$tarball
+rc=$?
 
 # Clean up
 rm $tarball
+cleanup
 
-# If a post-backup command is defined (eg: for cleanup)
-if [ -n "$AFTER_BACKUP_CMD" ]; then
-	eval "$AFTER_BACKUP_CMD"
+end_time=`date +%Y-%m-%d\\ %H:%M:%S\\ %Z`
+if [ $rc -ne 0 ]; then
+  echo "ERROR: Error uploading backup to S3"
+  echo -e "[$end_time] Backup failed\n\n"
+  exit $rc
+else
+  echo -e "[$end_time] Archive successfully uploaded to S3\n\n"
 fi
